f67a101cef5983459ad2485a4dcd8f980fd0587e
Nathan Laha <nlaha@outlook.com>
2020-10-28 00:00:45 -0700

    implement full oauth flow and URL parameters

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import "./Custom.css";
 
 import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { useHistory, useLocation } from "react-router-dom";
 
 // Material UI
 import Button from "@material-ui/core/Button";
@@ -11,6 +12,8 @@ import Typography from "@material-ui/core/Typography";
 import Container from "@material-ui/core/Container";
 
 import MicrosoftLogin from "react-microsoft-login";
+import queryString from "query-string";
+import axios from "axios";
 
 import {
   createMuiTheme,
@@ -39,6 +42,9 @@ const theme = createMuiTheme({
 // custom CSS
 const useStyles = makeStyles((theme) => ({}));
 
+var authData;
+var params;
+
 // Router
 function App() {
   return (
@@ -47,29 +53,40 @@ function App() {
         <Route exact path="/">
           <LandingPage />
         </Route>
+        <Route path="/register">
+          <LandingPage />
+        </Route>
         <Route path="/rules">
           <Rules />
         </Route>
         <Route path="/oauthCallback">
           <OAuthCallback />
         </Route>
+        <Route path="/registerSuccess">
+          <Success />
+        </Route>
+        <Route path="/error">
+          <Error />
+        </Route>
+        <Route path="/noToken">
+          <NoToken />
+        </Route>
       </Router>
     </ThemeProvider>
   );
 }
 
-var authData = "no data";
-
-const authHandler = (err, data) => {
-  console.log("Storing Auth ID" + data);
-  authData = data;
-  if (err) {
-    authData = err;
-  }
-};
-
 // Index Page
 function LandingPage() {
+  const location = useLocation();
+  const history = useHistory();
+  params = queryString.parse(location.search);
+
+  // exit out if we don't have a token
+  if (!params || !params.token) {
+    history.push("/noToken");
+  }
+
   const classes = useStyles();
   return (
     <div className="App">
@@ -104,9 +121,40 @@ function LandingPage() {
   );
 }
 
+function NoToken() {
+  return ShowMessage("Please use the link provided in the Minecraft Server");
+}
+
 // Rules Page
 function Rules() {
   const classes = useStyles();
+  const history = useHistory();
+
+  // exit out if we don't have a token
+  if (!params || !params.token) {
+    history.push("/noToken");
+  }
+
+  const authHandler = (err, data) => {
+    if (data) {
+      console.log("Storing Auth ID");
+      console.log(
+        "Account ID:" +
+          data.authResponseWithAccessToken.account.accountIdentifier
+      );
+      console.log(data);
+      authData = data;
+    } else {
+      history.push("/error");
+    }
+
+    if (err) {
+      authData = err;
+    }
+
+    history.push("/registerSuccess");
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -137,7 +185,7 @@ function Rules() {
           <MicrosoftLogin
             clientId="1ac69b12-d1f7-48cb-a361-e85487cefb4f"
             authCallback={authHandler}
-            redirectUri="https://mc.nlaha.com/oauthCallback"
+            redirectUri="https://f5024f3950b2.ngrok.io/oauthCallback"
           />
         </Container>
       </header>
@@ -145,8 +193,77 @@ function Rules() {
   );
 }
 
+function ShowMessage(message, messageSubtitle, messageSubtitle2) {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Container maxWidth="md">
+          <Typography variant="h3" className="pageTitle">
+            {message}
+          </Typography>
+          <Typography className="infoParagraph" variant="h6" gutterBottom>
+            {messageSubtitle}
+          </Typography>
+          <Typography className="infoParagraph" variant="h6" gutterBottom>
+            {messageSubtitle2}
+          </Typography>
+          <Link to="/">
+            <Button
+              className="large-btn"
+              size="large"
+              variant="contained"
+              color="primary"
+            >
+              Exit
+            </Button>
+          </Link>
+        </Container>
+      </header>
+    </div>
+  );
+}
+
+function Success() {
+  const history = useHistory();
+  if (!authData.authResponseWithAccessToken) {
+    return ShowMessage(
+      "Something went wrong, please try again from the original link!"
+    );
+  }
+
+  var registerData = {
+    OAuthData: authData,
+    OriginalMCToken: params.token,
+  };
+
+  axios.post(process.env.REACT_APP_MC_URL, { registerData }).then((res) => {
+    return ShowMessage(
+      `Sucessfully Registered`,
+      `SPS Account: ${authData.authResponseWithAccessToken.account.name}!`,
+      `MC Token: ${params.token}`
+    );
+  });
+
+  return ShowMessage(
+    "Can't connect to Minecraft, it might be offline, check back later!",
+    `SPS Account: ${authData.authResponseWithAccessToken.account.name}!`,
+    `MC Token: ${params.token}`
+  );
+}
+
+function Error() {
+  return ShowMessage(`Error :(`);
+}
+
 // OAuth Callback
 function OAuthCallback() {
+  const history = useHistory();
+  if (!authData) {
+    return ShowMessage(
+      "Something went wrong, please try again from the original link!"
+    );
+  }
+
   return <h1>Registered Sucessfully! {authData}</h1>;
 }
 
